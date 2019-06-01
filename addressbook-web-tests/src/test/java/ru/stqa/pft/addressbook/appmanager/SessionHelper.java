df2a725084f7f8bf917c47d4cb7c2b34376d7db0
Alexey <swtester@yahoo.com>
2019-06-01 15:58:18 +0400

    Пройден урок 26

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/SessionHelper.java
@@ -0,0 +1,19 @@
+package ru.stqa.pft.addressbook.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class SessionHelper extends HelperBase {
+
+  public SessionHelper(FirefoxDriver wd) {
+    super(wd);
+  }
+
+  public void login(String username, String password) {
+    type(By.name("user"), username);
+    type(By.name("pass"), password);
+    click(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Password:'])[1]/following::input[2]"));
+  }
+
+
+}
